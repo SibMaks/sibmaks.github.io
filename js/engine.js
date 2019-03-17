a8086a3110271f0edae3821bf2db069cb3a1e522
maksim.drobyshev <maksim.drobyshev@fxclub.com>
2019-03-17 23:51:33 +0700

    Refactor all

--- js/engine.js
+++ js/engine.js
@@ -1,148 +1,132 @@
-// позиция игрока, ускорение, поворот, вектор вращения
-let motion = {
-				airborne: false,
-				position: new THREE.Vector3(), velocity: new THREE.Vector3(),
-				direction: new THREE.Vector3(), spinning: new THREE.Vector2()
-			};
-
-motion.position.y = - 150;
-
-let keys = { SP: 32, W: 87, A: 65, S: 83, D: 68 };
-let keysPressed = {};
-	
-//Игровые объекты
-var objects = [];
-
-// сброс позиции игрока
-let resetPlayer = function (context) {
-	//motion.position.copy( context.camera.position );
-	context.camera.position.copy( motion.position );
-	if ( motion.position.y < - 123 ) {
-		motion.position.set( - 2, 7.7, 25 );
-		motion.velocity.multiplyScalar( 0 );
+let moveUp = (function () {
+	let fly = new Audio("audio/fly.mp3");
+	return function (bird) {
+		bird.y -= 25;
+		fly.play();
 	}
-};
+}) ();
 
-let keyboardControls = ( function () {
-	//let keysPressed = {};
-	( function ( watchedKeyCodes ) {
-		let handler = function ( down ) {
-			return function ( e ) {
-				let index = watchedKeyCodes.indexOf( e.keyCode );
-				if ( index >= 0 ) {
-					keysPressed[ watchedKeyCodes[ index ] ] = down;
-					e.preventDefault();
+const STATES = {PAUSE: 'PAUSE', OVER: 'OVER', PLAY: 'PLAY'};
+
+function restart(canvas, context) {
+	context.bird.x = 10;
+	context.bird.y = 150;
+	context.pipe.splice(0, context.pipe.length);
+
+	context.pipe.push(
+		{
+			x : canvas.width,
+			y : 0
+		}
+	);
+	context.score = 0;
+}
+
+$(function () {
+	let canvas = $("#game-canvas")[0];
+	let context = canvas.getContext("2d");
+	let fgXOffset = 0;
+
+	let gameContext = {
+		bird: {image: new Image()},
+		pipe: [],
+		score: 0,
+		state: STATES.PAUSE,
+		highScore: 0,
+		gravity: 1.5
+	};
+	gameContext.bird.image.src = "textures/bird.png";
+
+	// Массив блоков
+	restart(canvas, gameContext);
+
+	let bg = new Image();
+	bg.src = "textures/bg.png";
+
+	let fg = new Image();
+	fg.src = "textures/fg.png";
+
+	let pipeUp = new Image();
+	pipeUp.src = "textures/pipeUp.png";
+
+	let pipeBottom = new Image();
+	pipeBottom.src = "textures/pipeBottom.png";
+
+	// Звуковые файлы
+	let scoreAudio = new Audio("audio/score.mp3");
+
+	let gap = 90;
+
+	// При нажатии на какую-либо кнопку прыгаем
+	$(document).keydown(function (e) {
+		if(gameContext.state === STATES.PAUSE) {
+			gameContext.state = STATES.PLAY;
+		} else if(gameContext.state === STATES.OVER && e.key === 'Enter') {
+			gameContext.state = STATES.PLAY;
+		} else if(gameContext.state === STATES.PLAY) {
+			moveUp(gameContext.bird);
+		}
+	});
+
+	draw();
+
+	function draw() {
+		context.drawImage(bg, 0, 0);
+
+		if(gameContext.state === STATES.PLAY) {
+			for (let i = 0; i < gameContext.pipe.length; i++) {
+				context.drawImage(pipeUp, gameContext.pipe[i].x, gameContext.pipe[i].y);
+				context.drawImage(pipeBottom, gameContext.pipe[i].x, gameContext.pipe[i].y + pipeUp.height + gap);
+
+				gameContext.pipe[i].x--;
+
+				//если текущий блок появляется на экране
+				if (gameContext.pipe[i].x === (canvas.width / 2)) {
+					//делаем не сильно большой разброс
+					const min = 0.6;
+					const delta = Math.random() * (1 - min) + min;
+					gameContext.pipe.push({
+						x: canvas.width,
+						y: Math.floor(delta * pipeUp.height) - pipeUp.height
+					});
 				}
-			};
-		};
-		window.addEventListener( "keydown", handler( true ), false );
-		window.addEventListener( "keyup", handler( false ), false );
-	} )( [
-		keys.SP, keys.W, keys.A, keys.S, keys.D
-	] );
-	let forward = new THREE.Vector3();
-	let sideways = new THREE.Vector3();
-	return function (context) {
-		if ( motion.airborne ) {
-			return;
-		}
-		// move around
-		forward.set( Math.sin( context.camera.rotation.y ), 0, Math.cos( context.camera.rotation.y ) );
-		sideways.set( forward.z, 0, - forward.x );
-		forward.multiplyScalar( keysPressed[ keys.W ] ? - 0.1 : ( keysPressed[ keys.S ] ? 0.1 : 0 ) );
-		sideways.multiplyScalar( keysPressed[ keys.A ] ? - 0.1 : ( keysPressed[ keys.D ] ? 0.1 : 0 ) );
-		let combined = forward.add( sideways );
-		//if ( Math.abs( combined.x ) >= Math.abs( motion.velocity.x ) ) motion.velocity.x = combined.x;
-		//if ( Math.abs( combined.y ) >= Math.abs( motion.velocity.y ) ) motion.velocity.y = combined.y;
-		//if ( Math.abs( combined.z ) >= Math.abs( motion.velocity.z ) ) motion.velocity.z = combined.z;
-		//jump
-		let vy = keysPressed[ keys.SP ] ? 0.7 : 0;
-		//motion.velocity.y += vy;
-	};
-} )();
 
-let jumpPads = ( function () {
-	let pads = [ new THREE.Vector3( - 17.5, 8, - 10 ), new THREE.Vector3( 17.5, 8, - 10 ), new THREE.Vector3( 0, 8, 21 ) ];
-	let temp = new THREE.Vector3();
-	return function () {
-		if (motion.airborne ) {
-			return;
-		}
-		for ( let j = 0, n = pads.length; j < n; j ++ ) {
-			if ( pads[ j ].distanceToSquared( motion.position ) < 2.3 ) {
-				// calculate velocity towards another side of platform from jump pad position
-				temp.copy( pads[ j ] );
-				temp.y = 0;
-				temp.setLength( - 0.8 );
-				temp.y = 0.7;
-				motion.airborne = true;
-				motion.velocity.copy( temp );
-				break;
+				// Отслеживание прикосновений
+				if (gameContext.bird.x + gameContext.bird.image.width >= gameContext.pipe[i].x
+					&& gameContext.bird.x <= gameContext.pipe[i].x + pipeUp.width
+					&& (gameContext.bird.y <= gameContext.pipe[i].y + pipeUp.height
+						|| gameContext.bird.y + gameContext.bird.image.height >= gameContext.pipe[i].y + pipeUp.height + gap)
+					|| gameContext.bird.y + gameContext.bird.image.height >= canvas.height - fg.height) {
+					if (gameContext.highScore < gameContext.score) {
+						gameContext.highScore = gameContext.score;
+					}
+					gameContext.state = STATES.OVER;
+					restart(canvas, gameContext);
+					break;
+				}
+
+				//Если пролетели блок
+				if (gameContext.pipe[i].x === 5) {
+					gameContext.score++;
+					scoreAudio.play();
+				}
 			}
 		}
-	};
-} )();
 
-let applyPhysics = ( function () {
-	let timeStep = 5;
-	let raycaster = new THREE.Raycaster();
-	let prevTime = performance.now();
-	raycaster.ray.direction.set( 0, - 1, 0 );
-	return function ( dt, context ) {
-		//let platform = context.scene.getObjectByName( "platform", true );
-		if ( context.controls.isLocked ) {
-			raycaster.ray.origin.copy( context.controls.getObject().position );
-			raycaster.ray.origin.y -= 10;
-			var intersections = raycaster.intersectObjects( objects );
-			var onObject = intersections.length > 0;
-			var time = performance.now();
-			var delta = ( time - prevTime ) / 1000;
-			motion.velocity.x -= motion.velocity.x * 10.0 * delta;
-			motion.velocity.z -= motion.velocity.z * 10.0 * delta;
-			motion.velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-			motion.direction.z = Number( keysPressed[ keys.W ] ) - Number( keysPressed[ keys.S ] );
-			motion.direction.x = Number( keysPressed[ keys.A ] ) - Number( keysPressed[ keys.D ] );
-			motion.direction.normalize(); // this ensures consistent movements in all directions
-			if ( keysPressed[ keys.W ] || keysPressed[ keys.S ] ) motion.velocity.z -= motion.direction.z * 400.0 * delta;
-			if ( keysPressed[ keys.A ] || keysPressed[ keys.D ] ) motion.velocity.x -= motion.direction.x * 400.0 * delta;
-			if ( onObject === true ) {
-				motion.velocity.y = Math.max( 0, motion.velocity.y );
-				//canJump = true;
-			}
-			context.controls.getObject().translateX( motion.velocity.x * delta );
-			context.controls.getObject().translateY( motion.velocity.y * delta );
-			context.controls.getObject().translateZ( motion.velocity.z * delta );
-			if ( context.controls.getObject().position.y < 10 ) {
-				motion.velocity.y = 0;
-				context.controls.getObject().position.y = 10;
-				//canJump = true;
-			}
-			prevTime = time;
+		//Сбрасываем в ноль нижнюю часть
+		if(Math.abs(--fgXOffset) > fg.width) {
+			fgXOffset = 0;
 		}
-	};
-} )();
 
-$(function() {
-	let rendererContext = getRendererContext();
+		//рисуем "землю"
+		context.drawImage(fg, fgXOffset, canvas.height - fg.height);
+		context.drawImage(fg, fgXOffset + fg.width, canvas.height - fg.height);
 
-	let gameLoop = function ( dt ) {
-		resetPlayer(rendererContext);
-		keyboardControls(rendererContext);
-		//jumpPads();
-		applyPhysics( dt, rendererContext );
-		updateCamera(rendererContext.camera);
-		//console.log(motion.position.x + ", " + motion.position.y + ", " + motion.position.z);
-	};
+		//рисуем питцу
+		context.drawImage(gameContext.bird.image, gameContext.bird.x, gameContext.bird.y);
 
-	// start the game
-	let lastTimeStamp;
-	let render = function ( timeStamp ) {
-		let timeElapsed = lastTimeStamp ? timeStamp - lastTimeStamp : 0;
-		lastTimeStamp = timeStamp;
-		// call our game loop with the time elapsed since last rendering, in ms
-		gameLoop( timeElapsed );
-		rendererContext.renderer.render( rendererContext.scene, rendererContext.camera );
-		requestAnimationFrame( render );
-	};
-	requestAnimationFrame( render );
+		showStat(canvas, context, gameContext);
+
+		requestAnimationFrame(draw);
+	}
 });
--- js/renderer.js
+++ js/renderer.js
@@ -1,111 +1,32 @@
-// init 3D stuff
-function makePlatform( url ) {
-    let placeholder = new THREE.Object3D();
-    let loader = new THREE.ObjectLoader();
-    loader.load( url, function ( platform ) {
-        placeholder.add( platform );
-    } );
-    return placeholder;
-}
-
-let resize = function (renderer, camera) {
-    let viewport = gameViewportSize();
-    renderer.setSize( viewport.width, viewport.height );
-    camera.aspect = viewport.width / viewport.height;
-    camera.updateProjectionMatrix();
-};
-
-let updateCamera = ( function () {
-    let euler = new THREE.Euler( 0, 0, 0, 'YXZ' );
-    return function (camera) {
-        euler.x = camera.rotation.x;
-        euler.y = camera.rotation.y;
-        camera.quaternion.setFromEuler( euler );
-        camera.position.copy( motion.position );
-        camera.position.y += 3.0;
-    };
-} )();
-
-let gameViewportSize = function () {
-    return {
-        width: window.innerWidth, height: window.innerHeight
-    };
-};
-
-function makeFloor() {
-	// floor
-				var floorGeometry = new THREE.PlaneBufferGeometry( 2000, 2000, 100, 100 );
-				floorGeometry.rotateX( - Math.PI / 2 );
-				// vertex displacement
-				
-			var vertex = new THREE.Vector3();
-			var color = new THREE.Color();
-
-				var position = floorGeometry.attributes.position;
-				for ( var i = 0, l = position.count; i < l; i ++ ) {
-					vertex.fromBufferAttribute( position, i );
-					vertex.x += Math.random() * 20 - 10;
-					vertex.y += Math.random() * 2;
-					vertex.z += Math.random() * 20 - 10;
-					position.setXYZ( i, vertex.x, vertex.y, vertex.z );
-				}
-				floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
-				position = floorGeometry.attributes.position;
-				var colors = [];
-				for ( var i = 0, l = position.count; i < l; i ++ ) {
-					color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-					colors.push( color.r, color.g, color.b );
-				}
-				floorGeometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
-				var floorMaterial = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
-				var floor = new THREE.Mesh( floorGeometry, floorMaterial );
-	return floor;
-}
-
-function getRendererContext() {
-    let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    let controls = new THREE.PointerLockControls( camera );
-    let blocker = $( '#blocker' );
-    let instructions = $( '#instructions' );
-    instructions.on('click', function () {
-        controls.lock();
+function fillCenter(canvas, context, text) {
+    let textSize = context.measureText(text);
+    const lines = text.split('\n');
+    let y = (canvas.height - lines.length * 24) / 2;
+    lines.forEach(function(line) {
+        textSize = context.measureText(line);
+        context.fillText(line, (canvas.width - textSize.width) / 2, y);
+        y += 28;
     });
-    controls.addEventListener( 'lock', function () {
-        instructions.css('display', 'none');
-        blocker.css('display', 'none');
-    } );
-    controls.addEventListener( 'unlock', function () {
-        instructions.css('display', '');
-        blocker.css('display', 'block');
-    } );
+}
 
-    let scene = new THREE.Scene();
-    //Подгружаем SkyBox
-    let envMap = new THREE.CubeTextureLoader().load([
-        'textures/cube/skybox/px.jpg', // right
-        'textures/cube/skybox/nx.jpg', // left
-        'textures/cube/skybox/py.jpg', // top
-        'textures/cube/skybox/ny.jpg', // bottom
-        'textures/cube/skybox/pz.jpg', // back
-        'textures/cube/skybox/nz.jpg' // front
-    ]);
-    envMap.format = THREE.RGBFormat;
-    scene.background = envMap;
-    //Туман
-    scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
-    //scene.add(makePlatform(
-    //    'models/json/platform/platform.json'
-    //));
-	scene.add(makeFloor());
-    scene.add(controls.getObject());
+function fillMiddle(canvas, context, text, y) {
+    const textSize = context.measureText(text);
+    context.fillText(text, (canvas.width - textSize.width) / 2, y);
+}
 
-    let renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild(renderer.domElement);
-    window.addEventListener('resize', function() {resize(renderer, camera);}, false);
-
-    resize(renderer, camera);
-    return {renderer: renderer, scene: scene, camera: camera, controls: controls};
+function showStat(canvas, context, gameContext) {
+    if(gameContext.state === STATES.PLAY) {
+        gameContext.bird.y += gameContext.gravity;
+        context.fillStyle = "#000";
+        context.font = "24px 'Back to 1982'";
+        fillMiddle(canvas, context, gameContext.score, 28);
+        context.font = "12px 'Back to 1982'";
+        fillMiddle(canvas, context, gameContext.highScore, 54);
+    } else if(gameContext.state === STATES.OVER) {
+        context.font = "24px 'Back to 1982'";
+        fillCenter(canvas, context, "GAME OVER\nPRESS ENTER");
+        context.fillStyle = "#000";
+        context.font = "24px 'Back to 1982'";
+        fillMiddle(canvas, context, gameContext.highScore, canvas.height - 48);
+    }
 }
